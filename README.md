d1315158fb3874ea54d4dd7d80cb705374d73014
Ian Stahl <ianpstahl@gmail.com>
2017-09-20 16:18:18 -0700

    JS somewhat functional.

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Week_5_Homework_Trivia
+
+#Captain's Log Entry #1 9/19
+
+
+It's highkey awful.
+
+I would like to make several changes.
+
+Thing is, I was so fixated on making the Javascript work that I completely forgot about styling. 
+
+It's somewhat the same thing that happened with the previous assignment.
+
+Thing's I must learn:
+
+Perfection isn't attainable. 
+Sometimes, the best thing to do is to step away from the logic of it all and focus on something else. 
+
+SEGUE
+
+In that way there won't be empty time spaces in which I just look at the code and think how cool it would be if it worked properly.
+I will return to this assignment as quickly as possible so that I may make it the way I envisioned it.
+
+Lessons were learned in this assignment. Good ones and bad ones.
+
+Good Lessons: 
+
+Learned of template literals. Pretty neat. 
+And that's about it.
+
+
+
+Fixes Priority List:
+
+1. Make a timer that resets within each new slide.
+
+2. STYLE...FUCK.
+
+3. Make a better website. That's like a general rule. But this will be priority three. Because reasons. 
+
+4. Better progress management. It is possible. If I feel brain blocked, work on something else where progress CAN be made. 
+
+5. Do my Laundry.
+
+6. Get some sleep. Work is being annoying. Find a way to make work suck less of my time. 
+
+
+
---
+++ javascript.js
@@ -0,0 +1,379 @@
+//An Array of options and questions for those options.
+( function() {
+
+
+
+    var ff7 = [
+
+        {
+
+            question: "Which of these was weapons was NOT present in the main game?",
+
+            options: {
+                a: "Sapphire Weapon",
+                b: "Ruby Weapon",
+                c: "Ultimate Weapon",
+                d: "Emerald Weapon",
+                e: "Omega Weapon"
+            },
+
+            correctAnswer: "e",
+
+        },
+
+        {
+
+            question: "How many characters were in your party by the end of the game(including optional characters)?",
+
+            options: {
+                a: "7",
+                b: "4",
+                c: "8",
+                d: "9",
+                e: "3"
+            },
+
+            correctAnswer: "c",
+
+        },
+
+        {
+
+            question: "What is the name of Cloud Strifes most recognizable weapon?",
+
+
+            options: {
+
+                a: "Buster Blade",
+                b: "Crystal Blade",
+                c: "Epoch",
+                d: "Gun Blade",
+                e: "Wooden Sword"
+            },
+
+            correctAnswer: "a",
+        },
+
+
+        {
+
+            question: "What was the name of thr group trying to hunt you down?",
+
+            options: {
+
+                a: "Organization XIII",
+                b: "The Turks",
+                c: "ANBU",
+                d: "Secret Service",
+                e: "Imhotep"
+            },
+
+            correctAnswer: "b",
+        },
+
+        {
+
+            question: "How many forms did the final boss have?",
+
+            options: {
+
+                a: "1",
+                b: "2",
+                c: "3",
+                d: "4",
+                e: "5"
+            },
+
+            correctAnswer: "b",
+
+        },
+
+
+         {
+
+            question: "What materia does Aeris start off with when she joins your party?",
+
+            options: {
+                a: "Restore",
+                b: "Cure",
+                c: "Esuna",
+                d: "All of the Above",
+                e: "None of the Above"
+            },
+
+            correctAnswer: "e",
+
+        },
+
+        {
+
+            question: "What is the strongest summon in the game?",
+
+            options: {
+                a: "Bahamut",
+                b: "Alexandria",
+                c: "Odin",
+                d: "Knights Of The Round",
+                e: "Ifrit"
+            },
+
+            correctAnswer: "d",
+
+        },
+
+        {
+
+            question: "What type does the blue materia represent?",
+
+            options: {
+                a: "Summon",
+                b: "Support",
+                c: "Magic",
+                d: "Independent",
+                e: "Command"
+            },
+
+            correctAnswer: "b",
+
+        },
+
+        {
+
+            question: "Early in the game, Cloud is talked into doing drag to invade Don Corneo's mansion. To this end, what key item is obtained by besting the Big Bro in the squat thrust minigame?",
+
+            options: {
+                a: "Wig",
+                b: "Dyed Wig",
+                c: "Bikini Briefs",
+                d: "Perfume",
+                e: "Shiny"
+            },
+
+            correctAnswer: "b",
+
+        },
+
+        
+
+
+    ];
+
+    //Used to store DOM manipulation to these elements in variables.
+
+    //Timer
+    var number = 10;
+
+    var intervalId;
+
+    function timeRun() {
+
+    	intervalId = setInterval(minusTime, 1000);
+    }
+
+    function minusTime() {
+
+    	number--;
+
+    	$("#timeAvailable").html("<p>" + number + "</p>");
+
+    	if (number === 0) {
+
+    		timeStop();
+    		$("#timeAvailable").html("<p> Time's Up! </p>");
+    		nextSlide();
+    	}
+    }
+
+    function timeStop() {
+
+    	clearInterval(intervalId);
+    }
+
+
+
+    
+
+
+    //have a code block that actually runs the stupid fucking game. So help me god
+
+    function quizBuild() {
+
+
+
+        var audioElement = document.createElement("audio");
+        audioElement.setAttribute("src", "assets/mp3/lightwaves.mp3");
+
+        $(".theme-button").on("click", function() {
+            audioElement.play();
+        });
+
+
+        var outputHTML = [];
+
+        //this will run through the ff7 questions array one by one. Neato
+
+        ff7.forEach((currentQuestion, questionNumber) => {
+
+            var options = [];
+
+            for (letter in currentQuestion.options) {
+
+
+
+                options.push(
+                    `<label> 
+				<input type="radio"
+				 name="question${questionNumber}"
+				 value="${letter}">
+					${letter} : 
+					${currentQuestion.options[letter]}
+				</label>`
+
+
+                );
+
+            }
+
+            //this outputs it for DOM manipulation homeboy
+
+            outputHTML.push(
+
+                `
+				<div class="slide">
+                <div class="question"> ${currentQuestion.question} </div>
+				<div class="options"> ${options.join('')} </div>
+				</div>`
+
+
+            );
+
+
+
+        });
+
+        quizContainer.innerHTML = outputHTML.join("");
+        
+
+
+    };
+
+    
+
+    //My arch nemesis. RESULTS. work. PLEASE. 
+
+    function showResults() {
+
+        var answerContainers = quizContainer.querySelectorAll('.options');
+
+        var numCorrect = 0;
+
+        ff7.forEach((currentQuestion, questionNumber) => {
+
+            var answerContainer = answerContainers[questionNumber];
+            var selector =
+                `input[name=question${questionNumber}]:checked`;
+            var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+
+
+           
+
+             if (userAnswer === currentQuestion.correctAnswer) {
+
+                numCorrect++;
+
+                answerContainers[questionNumber].style.color = "green";
+            }
+else {
+   answerContainers[questionNumber].style.color = 'red';
+
+            }
+
+
+
+        });
+        resultsContainer.innerHTML = `${ numCorrect } out of  ${ff7.length}`;
+
+
+    };
+
+  
+
+
+    function slideShow(num) {
+
+    	slides[currentSlide].classList.remove('active-slide');
+  slides[num].classList.add('active-slide');
+    		currentSlide = num;
+
+    		if (currentSlide === 0) {
+
+    			lastButton.style.display = "none";
+    		} else {
+
+    			lastButton.style.display = "inline-block";
+    		};
+
+    		if ( currentSlide === slides.length- 1 ) {
+
+    			nextButton.style.display = "none";
+    			submission.style.display = "inline-block";
+    		} else {
+
+    			nextButton.style.display = "inline-block";
+    			submission.style.display = "none";
+    		};
+
+
+    };
+
+
+    function nextSlide() {
+    	slideShow(currentSlide+ 1);
+
+    };
+
+    function lastSlide() {
+    	slideShow(currentSlide- 1);
+    };
+
+
+    var quizContainer = document.getElementById("quiz");
+    var resultsContainer = document.getElementById("results");
+    var submission = document.getElementById("submit");
+  
+
+    quizBuild();
+
+      var lastButton = document.getElementById("lastQuestion");
+    var nextButton = document.getElementById("followingQuestion");
+    var slides = document.querySelectorAll(".slide");
+
+
+    //Variable to start with the first question of the question array. Will be used for
+    // a For Loop.
+
+
+    var currentSlide = 0;
+
+     slideShow(0);
+
+
+    submission.addEventListener("click", showResults);
+    nextButton.addEventListener("click", nextSlide);
+    lastButton.addEventListener("click", lastSlide);
+
+
+
+
+
+})();
+
+
+
+
+//Would like difficulty levels in terms of ambiguity and additions of pictures or not. 
+
+//There will be several silly things that will occur depending on a few things. Just get it right.
+
+//Need to have it times so that player only has 10 seconds to answer properly to said questions.
